a8ce04339dbf0d49067cfbad9d4247719ba10b8c
Lev <levpourel0@gmail.com>
2023-01-15 06:47:25 +0200

    Tasks

---
+++ Напишите функцию неглубокого копирования массива, учитывая, что исходный массив может быть произвольной длины, но не глубже массива в массиве.js
@@ -0,0 +1,18 @@
+'use strict';
+const arr1 = [[1,2], [3,4]];
+const arr2 = aCopy(arr1);
+
+arr1.push([5,6]);
+
+console.log(arr1); // [[1,2], [3,4], [5,6]]
+console.log(arr2); // [[1,2], [3,4]]
+
+// --
+
+const arr1 = [['a','b', 'c','d'], ['e', 'f'], ['g', 'h', 'i']];
+const arr2 = aCopy(arr1);
+
+arr1.unshift(['z']);
+
+console.log(arr1); // [['z'], ['a','b', 'c','d'], ['e', 'f'], ['g', 'h', 'i']]
+console.log(arr2); // [['a','b', 'c','d'], ['e', 'f'], ['g', 'h', 'i']]
---
+++ Функция берёт массив и возвращает массив значений определённого поля.js
@@ -0,0 +1,12 @@
+const chars = [
+    { 'name': 'tony', 'age': 20 },
+    { 'name': 'feel', 'age': 30 }
+];
+function pluck(massive, field) {
+    let newMassive = [];
+    for(let i = 0; i<massive.length;i++) {
+        newMassive.push(massive[i][field]);
+    }
+    return newMassive;
+}
+console.log(pluck(chars, 'name')); // ['tony', 'feel']
