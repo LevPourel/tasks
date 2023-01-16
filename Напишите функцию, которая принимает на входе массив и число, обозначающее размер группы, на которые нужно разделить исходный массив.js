9a6a92a9a599df995ea719290f9e64e616e537d4
Lev <levpourel0@gmail.com>
2023-01-16 09:17:05 +0200

    Task 3 done

--- Напишите функцию, которая принимает на входе массив и число, обозначающее размер группы, на которые нужно разделить исходный массив.js
+++ Напишите функцию, которая принимает на входе массив и число, обозначающее размер группы, на которые нужно разделить исходный массив.js
@@ -1,7 +1,14 @@
 //Напишите функцию, которая принимает на входе массив и число, обозначающее размер группы, на которые нужно
 // разделить исходный массив
-function splitArray(massive, number){
-
+function splitArray(massive, number) {
+    let iteration = massive.length/number;
+    for(let i = 0; i<iteration; i++) {
+    let a = massive.slice(0, number);
+    massive.shift()
+        massive.shift()
+    massive.push(a);
+    }
+    return massive;
 }
 const arr1 = splitArray(['a', 'b', 'c', 'd', 'e', 'f'], 2);
 console.log(arr1); // [['a', 'b'], ['c', 'd'], ['e', 'f']]
