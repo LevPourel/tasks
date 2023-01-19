786e99b03346247dfae0de23a09333969a0ef04b
Lev <levpourel0@gmail.com>
2023-01-19 19:01:23 +0200

    Task 20 alpha

--- Task 20.js
+++ Task 20.js
@@ -2,13 +2,22 @@
 const arr = [7, 'z', 'z', 'z', 3, 7, 'z', 7, 'z', 3, 5, 9, 7];
 function fn(arr) {
     let newArr = arr.slice();
+    let target;
+    let c = [];
     newArr.sort()
     const counter = arr.reduce(function (acc , item) {
-
-        if(acc.indexOf(item) === -1) {
-            acc.push(item)
-        }
-    }, [])
-    return counter
+        acc[item] = (acc[item] || 0) +1;
+        return acc
+    }, {})
+    target = Object.entries(counter)
+    target.sort();
+    let a = target.reduce( function (acc, item, index, arr) {
+        let maxNum = item[1];
+        c.push(maxNum)
+        index = Math.max.apply(null, c)
+        console.log(target.indexOf(index))
+        return acc
+    },target)
+    return a
 }
 console.log(fn(arr)); // 'z'
